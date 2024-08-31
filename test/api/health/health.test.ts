8f4db5df4a9a00fa19f9b38e9177c8e5ff74157b
RaquelGCDev <raquel.garrido.dev@gmail.com>
2024-08-31 21:00:37 +0200

    vitest config and first test

--- test/api/health/health.test.ts
+++ test/api/health/health.test.ts
@@ -1,10 +1,11 @@
-import request from "supertest";
+import request from 'supertest';
+import { describe, it, expect } from 'vitest';
 import app from "../../../src/api/app";
-
 
 describe('Health', () => {
     it('Should get a 200 response', async () => {
-        const response = await request(app).get('/').send();
+        const response = await request(app)
+            .get('/');
 
         expect(response.statusCode).toBe(200);
     })
